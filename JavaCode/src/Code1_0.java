9f91fde32b66d16799e29029e5d9be27eb642269
Jason <jason.code.startup@gmail.com>
2014-10-18 13:26:51 -0700

    Add Code1_0.java

---
+++ JavaCode/src/Code1_0.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+public class Code1_0 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Learning HashMap");
+		
+		HashMap<Integer, String> map = new HashMap<Integer, String>();
+		map.put(21, "Twenty One");
+		map.put(1, "One");
+		System.out.println("Size of the Map is: " + map.size());
+		System.out.println(map.size());
+		System.out.println(map.get(1));
+	}
+
+}
