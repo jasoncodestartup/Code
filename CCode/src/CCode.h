b2599e921449718172ced821a2afc0103b7a6931
Jason <jason.code.startup@gmail.com>
2014-10-18 15:51:05 -0700

    Added Code1_4.c

---
+++ CCode/src/CCode.h
@@ -0,0 +1,19 @@
+/*
+ * CCode.h
+ *
+ *  Created on: Oct 18, 2014
+ *      Author: jmp
+ */
+
+#ifndef CCODE_H_
+#define CCODE_H_
+
+void Code1_2();
+void Code1_3();
+void Code1_4();
+
+void reverse(char* str);
+int isPermutation(char* str1, char* str2);
+void replaceSpaces(char* str, int len);
+
+#endif /* CCODE_H_ */
