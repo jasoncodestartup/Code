5257070a38c1d6b4758fb9ed8a4d374b7a995a34
Jason <jason.code.startup@gmail.com>
2014-10-18 14:15:54 -0700

    Corrected name: Code1_1.c --> Code1_2.c

---
+++ CCode/src/Code1_2.c
@@ -0,0 +1,36 @@
+/*
+ * Code1_1.c
+ *
+ *  Created on: Oct 18, 2014
+ *      Author: jmp
+ */
+#include <stdio.h>
+
+void reverse(char* str)
+{
+	// check for empty string
+	if (!str) return;
+
+	// find the last index of the string
+	char* end = str;
+	while(*end) end++;
+	end--;
+
+	char tmp;
+	while (str < end){
+		tmp = *str;
+		*str++ = *end;
+		*end-- = tmp;
+	}
+}
+
+
+int main(){
+	char str[14] = "Hello, World!";
+
+	printf("Original: %s\r", str);
+	reverse(str);
+	printf("Reversed: %s\r", str);
+
+	return 1;
+}
