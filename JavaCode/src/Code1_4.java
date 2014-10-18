c446237f9c10127c1c2f657c4d0893e861a2f7bf
Jason <jason.code.startup@gmail.com>
2014-10-18 16:30:38 -0700

    Adding Code1_4.java

---
+++ JavaCode/src/Code1_4.java
@@ -0,0 +1,12 @@
+
+public class Code1_4 {
+	public static void replaceSpaces(char[] str, int len){
+		// pretty much the same as C
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
