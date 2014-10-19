54cebccc10326ea08927562aef9f354b9ec3f35b
Jason <jason.code.startup@gmail.com>
2014-10-18 20:43:07 -0700

    Code1_7.java updated: int[][] mtx, mtx.length, mtx[0].length

--- JavaCode/src/Code1_7.java
+++ JavaCode/src/Code1_7.java
@@ -1,12 +1,12 @@
 
 public class Code1_7 {
-	public static void zeroOut(int[][] mtx, int M, int N){
-		boolean[] rows = new boolean[M];
-		boolean[] cols = new boolean[N];
+	public static void zeroOut(int[][] mtx){
+		boolean[] rows = new boolean[mtx.length];
+		boolean[] cols = new boolean[mtx[0].length];
 		
 		// find rows and columns to zero out
-		for (int r = 0; r < M; r++){
-			for (int c = 0; c < N; c++){
+		for (int r = 0; r < mtx.length; r++){
+			for (int c = 0; c < mtx[0].length; c++){
 				if (mtx[r][c] == 0){
 					rows[r] = true;
 					cols[c] = true;
@@ -15,8 +15,8 @@ public class Code1_7 {
 			}
 		}
 		
-		for (int r = 0; r < M; r++){
-			for (int c = 0; c < N; c++){
+		for (int r = 0; r < mtx.length; r++){
+			for (int c = 0; c < mtx[0].length; c++){
 				if (rows[r] || cols[c]){
 					mtx[r][c] = 0;
 				}
@@ -29,7 +29,7 @@ public class Code1_7 {
 		int mtx[][] = { {1, 2, 3, 0, 4},	// 0 0 0 0 0
 						{0, 1, 2, 2, 2},	// 0 0 0 0 0
 						{1, 1, 1, 1, 1} };	// 0 1 1 0 1
-		zeroOut(mtx, 3, 5);
+		zeroOut(mtx);
 		
 		for (int r = 0; r < 3; r++){
 			for (int c = 0; c < 5; c++){
